f5535239ccc49a3cfb5e133d24a39f9c3c0c3ac3
Flavius <heyerdinc@gmail.com>
2017-10-23 16:50:29 +0300

    Heap implementation
    changeKey heapify case : needs to update heapRank properly

--- Heap.c
+++ Heap.c
@@ -2,9 +2,88 @@
 // Created by Flavius on 17.10.2017.
 //
 
+#include <stdlib.h>
 #include "Heap.h"
 
-struct HeapNode{
+typedef struct HeapNode{
 	int key;
 	int vertexNo;
-};
+} HeapNode;
+
+HeapNode* heap;
+int size;
+
+void heapify(int index) {
+	int currentIndex;
+	int largest;
+	int stable;
+
+	currentIndex = index;
+	stable = 0;
+	while (!stable) {
+		//check left existence and compare
+		if(2 * currentIndex <= size && heap[2 * currentIndex].key > heap[currentIndex].key) {
+			largest = 2 * currentIndex;
+			stable = 0;
+		}
+		else {
+			largest = currentIndex;
+			stable = 1;
+		}
+		//check right existence and compare
+		if(2 * currentIndex + 1 <= size && heap[2 * currentIndex + 1].key > heap[largest].key) {
+			largest = 2 * currentIndex + 1;
+			stable = 0;
+		}
+
+		//not stable, swap and loop
+		if( largest != currentIndex) {
+			swapHeapNode(&heap[largest], &heap[currentIndex]);
+		}
+		//stable, terminate
+		else {
+			stable = 1;
+		}
+
+	}
+}
+
+int extractMax() {
+	size = size - 1;
+	swapHeapNode(&heap[0], &heap[size]);
+	heapify(0);
+	return heap[size].vertexNo;
+}
+
+void buildHeap(int* keys, int* vertexNumbers, int length) {
+	int i;
+
+	heap = malloc( sizeof(HeapNode) * length);
+	for( i = 0; i < length; i++) {
+		heap[i].key = keys[i];
+		heap[i].vertexNo = vertexNumbers[i];
+	}
+}
+
+void swapHeapNode(HeapNode* h1, HeapNode* h2) {
+	HeapNode* swapSpace;
+
+	*swapSpace = *h1;
+	*h1 = *h2;
+	*h2 = *swapSpace;
+}
+
+void changeKey(int keyChange, int* heapRank) {
+	heap[*heapRank].key += keyChange;
+	//change is positive, try to move up
+	if(keyChange > 0) {
+		while( heap[*heapRank].key > heap[(*heapRank) / 2].key) {
+			swapHeapNode( &heap[*heapRank], &heap[(*heapRank) / 2]);
+			*heapRank = *heapRank / 2;
+		}
+	}
+	//change is negative, try to go down
+	else {
+		heapify(&heapRank);
+	}
+}
--- Heap.h
+++ Heap.h
@@ -5,4 +5,9 @@
 #ifndef PARTITION_HEAP_H
 #define PARTITION_HEAP_H
 
+void buildMaxHeap(int* keys, int* vertexNumbers, int length);
+void changeKey(int keyChange, int* heapRank);
+int extractMax();
+void buildHeap(int* keys, int* vertexNumbers, int length);
+
 #endif //PARTITION_HEAP_H
